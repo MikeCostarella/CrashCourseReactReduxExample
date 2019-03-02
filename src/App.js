9cabfd0a376ece11398ddae549f59fd926b9c035
Mike Costarella <mcosti@sprynet.com>
2019-03-02 01:04:30 -0500

    Updated the app title to include redux

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Welcome to</h1>
+            <h1 className="App-title">React/Redux</h1>
           </header>
           <PostForm />
           <hr />
